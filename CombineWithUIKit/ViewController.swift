734eee5b293c1c2b7f193d560fa21cdf27496f4b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-13 14:58:16 +0300

    init

--- CombineWithUIKit/ViewController.swift
+++ CombineWithUIKit/ViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var purcheseButton: UIButton!
+    
+    private var cancellable: AnyCancellable?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        cancellable = NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: priceTextField)
+            .map { $0.object as? UITextField}
+            .compactMap { $0?.text }
+            .map { str -> Bool in
+                if let number = Double(str) {
+                    return number > 10
+                } else {
+                    return false
+                }
+            }
+            .assign(to: \.isEnabled, on: purcheseButton)
     }
 
 
